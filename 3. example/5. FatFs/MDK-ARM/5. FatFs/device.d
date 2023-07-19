3b92622996a1970f73ca8fb06fce22e02944598e
nayooooo <YEWANhup@outlook.com>
2023-07-20 02:26:36 +0800

    尝试嵌入FatFs失败

---
+++ 3. example/5. FatFs/MDK-ARM/5. FatFs/device.d
@@ -0,0 +1,4 @@
+5.\ fatfs/device.o: ..\Drivers\BSP\device\device.c \
+  ..\Drivers\BSP\device\device.h \
+  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # STM32G0-Flash
+
 用于测试Flash
+
+# 注意 #
+
+FatFs工程存在问题
